bb9fa8d90153033dca21390a5d4fae05540ec9d4
Th3a-Ft <thea.fort@le-campus-numerique.fr>
2025-05-19 16:39:17 +0200

    Itération 2 - Première page PHP

---
+++ item.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Item boutique</title>
+</head>
+
+<body>
+    <?php 
+    $name="Air Jordan";
+    $price=149.99;
+    $source="https://cdn.pixabay.com/photo/2015/10/29/01/24/shoes-1011596_960_720.jpg";
+    
+    echo "
+    <h1>$name</h1>
+    <p>$price<p>
+    <img src=$source alt='Basket Air Jordan'>
+    " ;
+    
+    ?>
+
+</body>
+
+</html>
